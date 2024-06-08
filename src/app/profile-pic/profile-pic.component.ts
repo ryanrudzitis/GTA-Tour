55b231dcfcfe8cb0feb08e26f15e0ea3ac4c4f29
ryanrudzitis <ryanrudzitis@icloud.com>
2024-06-07 18:57:26 -0700

    added profile pic to player page

--- src/app/profile-pic/profile-pic.component.ts
+++ src/app/profile-pic/profile-pic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-pic',
@@ -9,5 +9,7 @@ export class ProfilePicComponent {
   @Input() pic: string | null = null;
   @Input() firstName: string | null = null;
   @Input() lastName: string | null = null;
+  @Input() size: string | null = null;
+  @Input() textSize: string | null = null;
 
 }
